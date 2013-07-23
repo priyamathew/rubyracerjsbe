e94414681d863e3e095d451e84b9871fa3838525
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-23 10:01:45 -0500

    fixed validation for 2 players

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,19 @@
+# class MyValidator < ActiveModel::Validator
+#   def validate_max_players_have_not_been_reached
+#       errors.add_to_base("This is only a two player game.") unless players.size < 2
+#   end
+# end
+
 class Game < ActiveRecord::Base
+  
+  validate :validate_max_users_have_not_been_reached
   has_many :games_players
   has_many :players, through: :games_players
+  has_one  :player
 
-  # Remember to create a migration!
+  def validate_max_users_have_not_been_reached
+    errors.add(:players, "This is a 2 player game.") unless players.size < 3
+  end
+
 end
+
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,5 @@
-# class MyValidator < ActiveModel::Validator
-#   def validate_max_players_have_not_been_reached
-#       errors.add_to_base("This is only a two player game.") unless players.size < 2
-#   end
-# end
-
-
 class Player < ActiveRecord::Base
-  # include ActiveModel::Validations
-  # validates_with MyValidator
+  
 
   has_many :games_players
   has_many :games, through: :games_players
